b0316a1ac1a6b4744db364f513cf344ea7760dc7
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-04-16 09:22:53 +0200

    Allow to edit notes

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
@@ -36,3 +36,15 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+
+@views.route('/edit-note/<int:id_>', methods=['GET', 'POST'])
+@login_required
+def edit_note(id_):
+    note = Note.query.get(id_)
+    if request.method == 'POST':
+        note.data = request.form.get('note')
+        db.session.commit()
+        return redirect(url_for('views.home'))
+
+    return render_template('update.html', note=note, user=current_user)
